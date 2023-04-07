0b13263a97a04700013b0c4e957bf9652d2a046c
Robin <lopfi95@gmail.com>
2023-04-07 21:45:59 +0200

    🐛 some bug fixes

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ const { getConfig } = require("@confly-dev/confly-js");
 const config = require("./config.json");
 
 const client = new Discord.Client({
-    intents: ['Guilds',
-        "DirectMessages",
-        'GuildMessages', "MessageContent"],
-    partials: [Discord.Partials.Message, Discord.Partials.Channel]
+  intents: ["Guilds", "DirectMessages", "GuildMessages", "MessageContent"],
+  partials: [Discord.Partials.Message, Discord.Partials.Channel],
 });
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
+  getConfig(config.confly_token).then((conflyConfig) => {
+    console.log("Current confly config: ");
+    console.log(conflyConfig);
+  });
 });
 
 client.on("messageCreate", async (msg) => {
-    const conflyConfig = await getConfig(config.confly_token);
+  if (msg.author.bot) return;
+  const conflyConfig = (await getConfig(config.confly_token)).Main.config;
 
-    if (!conflyConfig.enabled) return;
-    if (msg.content === conflyConfig.trigger) {
-        msg.reply(conflyConfig.response);
-    }
+  if (!conflyConfig.enabled) return;
+  if (msg.content === conflyConfig.trigger) {
+    msg.reply(conflyConfig.response);
+  }
 });
 
 client.login(config.discord_token);
